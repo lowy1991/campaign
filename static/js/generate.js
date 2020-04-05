569215fdc0ee06bc4f9ab7858f2aeabf285d971e
Lionel <lionelowy@hotmail.com>
2020-04-05 18:35:27 +0800

    init

---
+++ static/js/generate.js
@@ -0,0 +1,17 @@
+function loadPicture() {
+	var maxColumn	= 100;
+	var maxRow		= 20;
+	for (i = 1; i <=maxRow; i++) {
+		$('#chart').append(`<div id="row-${i}"></div>`)
+		for (j = 1; j <= maxColumn; j++) {
+			$(`#row-${i}`).append(`<img src="static/img/row-${i}-col-${j}.jpg" class="tile" id="row-${i}-col-${j}"/>`);
+			if (j <= 5) {
+				$(`#row-${i}-col-${j}`).css('filter', 'grayscale(100%)');
+			}
+		}
+	}
+}
+
+$(document).ready(function() {
+	loadPicture();
+});
