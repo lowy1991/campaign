67b37990811e2cbab0e441b1672fbbdb0498ac3c
Lionel <lionelowy@hotmail.com>
2020-04-05 21:11:07 +0800

    color

--- static/js/generate.js
+++ static/js/generate.js
@@ -1,8 +1,24 @@
-function loadPicture() {
-	var maxColumn	= 100;
-	var maxRow		= 20;
+var hoursCompleted	= 5500; 
+var maxColumn		= 20;
+var maxRow			= 100;
+
+function update() {
+	var rate			= 10;
+	var tilesCompleted	= hoursCompleted/rate;
+	$('#hours').text(hoursCompleted);
 	for (i = 1; i <=maxRow; i++) {
-		$('#chart').append(`<div id="row-${i}"></div>`)
+		for (j = 1; j <= maxColumn; j++) {
+			if (tilesCompleted > 0) {
+				$(`#row-${i}-col-${j}`).css('filter', 'grayscale(0%)');
+				tilesCompleted--;
+			}
+		}
+	}
+}
+
+function loadPicture() {
+	for (i = 1; i <=maxRow; i++) {
+		$('#chart').append(`<div class="picture" id="row-${i}"></div>`)
 		for (j = 1; j <= maxColumn; j++) {
 			$(`#row-${i}`).append(`<img src="static/img/row-${i}-col-${j}.jpg" class="tile" id="row-${i}-col-${j}"/>`);
 			$(`#row-${i}-col-${j}`).css('filter', 'grayscale(100%)');
@@ -12,4 +28,5 @@ function loadPicture() {
 
 $(document).ready(function() {
 	loadPicture();
+	update();
 });
