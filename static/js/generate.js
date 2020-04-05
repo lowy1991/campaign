1c88ea5427d9d41c2e2fd1a9e96b42deedf031f5
Lionel <lionelowy@hotmail.com>
2020-04-05 23:49:16 +0800

    color

--- static/js/generate.js
+++ static/js/generate.js
@@ -2,33 +2,26 @@ var hoursCompleted	= 5500;
 var maxColumn		= 20;
 var maxRow			= 100;
 
-function update() {
+function loadPicture() {
 	var rate			= 10;
 	var tilesCompleted	= hoursCompleted/rate;
 	$('#hours').text(hoursCompleted);
-	for (i = 1; i <=maxRow; i++) {
-		for (j = 1; j <= maxColumn; j++) {
-			if (tilesCompleted > 0) {
-				$(`#row-${i}-col-${j}`).css('filter', 'grayscale(0%)');
-				$(`#row-${i}-col-${j}`).css('opacity', '1.0');
-				tilesCompleted--;
-			}
-		}
-	}
-}
-
-function loadPicture() {
+	rowloop:
 	for (i = 1; i <=maxRow; i++) {
 		$('#chart').append(`<div class="picture" id="row-${i}"></div>`)
 		for (j = 1; j <= maxColumn; j++) {
-			$(`#row-${i}`).append(`<img src="static/img/row-${i}-col-${j}.jpg" class="tile" id="row-${i}-col-${j}"/>`);
-			$(`#row-${i}-col-${j}`).css('filter', 'grayscale(100%)');
-			$(`#row-${i}-col-${j}`).css('opacity', '0.1');
+			if (tilesCompleted > 0) {
+				$(`#row-${i}`).append(`<img src="static/img/row-${i}-col-${j}.jpg" class="tile" id="row-${i}-col-${j}"/>`);
+				$(`#row-${i}-col-${j}`).css('opacity', '0.75');
+				tilesCompleted--;
+			}
+			else {
+				break rowloop;
+			}
 		}
 	}
 }
 
 $(document).ready(function() {
 	loadPicture();
-	update();
 });
